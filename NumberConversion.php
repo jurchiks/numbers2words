135c7d179c6af81e96e2ec23c735e1b1872dc4ca
Juris <jurchiks101@gmail.com>
2013-07-27 18:51:27 +0300

    Added currencyToWords() method, fixed some stuff. Still not nearly perfect, for example, in Russian the word "thousand" is feminine, but that isn't taken into consideration in every case at the moment, only kopecks are fixed. TODO: make this thing modular, one language per file with their underlying implementation of parsing into numbers, and use the main class as a gateway to access them.

--- NumberConversion.php
+++ NumberConversion.php
@@ -92,6 +92,31 @@ class NumberConversion
 		)
 	);
 	
+	private static $singleDigitsFeminine = array(
+		'lv' => array(
+			'1' => 'viena',
+			'2' => 'divas',
+			'3' => 'trīs',
+			'4' => 'četras',
+			'5' => 'piecas',
+			'6' => 'sešas',
+			'7' => 'septiņas',
+			'8' => 'astoņas',
+			'9' => 'deviņas'
+		),
+		'ru' => array(
+			'1' => 'одна',
+			'2' => 'две',
+			'3' => 'три',
+			'4' => 'четыре',
+			'5' => 'пять',
+			'6' => 'шесть',
+			'7' => 'семь',
+			'8' => 'восемь',
+			'9' => 'девять'
+		)
+	);
+	
 	private static $teens = array(
 		'11' => array(
 			'en' => 'eleven',
@@ -323,11 +348,221 @@ class NumberConversion
 		'es' => ' con '
 	);
 	
+	private static $currencies = array(
+		'USD' => array( // currency code
+			'lv' => array( // language
+				'1' => array( // before decimal
+					'1' => 'dolārs', // 1
+					'2' => 'dolāri', // 2-9
+					'3' => 'dolāru' // 0, 10-20, 30, 40...
+				),
+				'2' => array( // after decimal
+					'1' => 'cents',
+					'2' => 'centi',
+					'3' => 'centu'
+				)
+			),
+			'ru' => array(
+				'1' => array(
+					'1' => 'доллар', // 1
+					'2' => 'доллара', // 2-4
+					'3' => 'долларов' // 0, 5-20, 30, 40...
+				),
+				'2' => array(
+					'1' => 'цент',
+					'2' => 'цента',
+					'3' => 'центов'
+				)
+			),
+			'en' => array(
+				'1' => array(
+					'1' => 'dollar',
+					'2' => 'dollars',
+					'3' => 'dollars'
+				),
+				'2' => array(
+					'1' => 'cent',
+					'2' => 'cents',
+					'3' => 'cents'
+				)
+			),
+			// TODO lt/es
+		),
+		'EUR' => array(
+			'lv' => array(
+				'1' => array(
+					'1' => 'eiro',
+					'2' => 'eiro',
+					'3' => 'eiro'
+				),
+				'2' => array(
+					'1' => 'cents',
+					'2' => 'centi',
+					'3' => 'centu'
+				)
+			),
+			'ru' => array(
+				'1' => array(
+					'1' => 'евро',
+					'2' => 'евро',
+					'3' => 'евро'
+				),
+				'2' => array(
+					'1' => 'цент',
+					'2' => 'цента',
+					'3' => 'центов'
+				)
+			),
+			'en' => array(
+				'1' => array(
+					'1' => 'euro',
+					'2' => 'euro',
+					'3' => 'euro'
+				),
+				'2' => array(
+					'1' => 'cent',
+					'2' => 'cents',
+					'3' => 'cents'
+				)
+			),
+			// TODO lt/es
+		),
+		'LVL' => array(
+			'lv' => array(
+				'1' => array(
+					'1' => 'lats',
+					'2' => 'lati',
+					'3' => 'latu'
+				),
+				'2' => array(
+					'1' => 'santīms',
+					'2' => 'santīmi',
+					'3' => 'santīmu'
+				)
+			),
+			'ru' => array(
+				'1' => array(
+					'1' => 'лат',
+					'2' => 'лата',
+					'3' => 'лат'
+				),
+				'2' => array(
+					'1' => 'сантим',
+					'2' => 'сантима',
+					'3' => 'сантимов'
+				)
+			),
+			'en' => array(
+				'1' => array(
+					'1' => 'lat',
+					'2' => 'lats',
+					'3' => 'lats'
+				),
+				'2' => array(
+					'1' => 'santim',
+					'2' => 'santims',
+					'3' => 'santims'
+				)
+			),
+			// TODO lt/es
+		),
+		'LTL' => array(
+			'lv' => array(
+				'1' => array(
+					'1' => 'lits',
+					'2' => 'liti',
+					'3' => 'litu'
+				),
+				'2' => array(
+					'1' => 'cents',
+					'2' => 'centi',
+					'3' => 'centu'
+				)
+			),
+			'ru' => array(
+				'1' => array(
+					'1' => 'лит',
+					'2' => 'лита',
+					'3' => 'литов'
+				),
+				'2' => array(
+					'1' => 'цент',
+					'2' => 'цента',
+					'3' => 'центов'
+				)
+			),
+			'en' => array(
+				'1' => array(
+					'1' => 'litas',
+					'2' => 'litai',
+					'3' => 'litai'
+				),
+				'2' => array(
+					'1' => 'centas',
+					'2' => 'centai',
+					'3' => 'centai'
+				)
+			)
+			// TODO lt/es
+		),
+		'RUR' => array(
+			'lv' => array(
+				'1' => array(
+					'1' => 'rublis',
+					'2' => 'rubļi',
+					'3' => 'rubļu'
+				),
+				'2' => array(
+					'1' => 'kapeika',
+					'2' => 'kapeikas',
+					'3' => 'kapeiku'
+				)
+			),
+			'ru' => array(
+				'1' => array(
+					'1' => 'рубль',
+					'2' => 'рубля',
+					'3' => 'рублей'
+				),
+				'2' => array(
+					'1' => 'копейка',
+					'2' => 'копейки',
+					'3' => 'копеек'
+				)
+			),
+			'en' => array(
+				'1' => array(
+					'1' => 'ruble',
+					'2' => 'rubles',
+					'3' => 'rubles'
+				),
+				'2' => array(
+					'1' => 'kopek',
+					'2' => 'kopek',
+					'3' => 'kopek'
+				)
+			),
+			// TODO lt
+			'es' => array(
+				'1' => array(
+					'1' => 'rublo',
+					'2' => 'rublos',
+					'3' => 'rublos'
+				),
+				'2' => array(
+					'1' => 'kopek',
+					'2' => 'kopeks',
+					'3' => 'kopeks'
+				)
+			),
+		)
+	);
+	
 	private static $es_twentysomething = 'veinti';
 	// just plain one hundred (100) is written differently in Spanish
 	// than when used in conjunction with other numbers like 101
 	private static $es_hundred = 'cien';
-	private static $es_thousand = 'mil';
+	private static $es_thousand = 'mil'; // TODO use this
 	private static $en_hundred = ' hundred';
 	
 	/**
@@ -342,32 +577,10 @@ class NumberConversion
 	 */
 	public static function numberToWords($number, $language, $beforeDecimal = '', $atEnd = '')
 	{
-		if (!is_numeric($number))
-		{
-			error_log(__METHOD__ . ': invalid number specified.');
-			return 'null';
-		}
+		$check = self::checkParameters($number, $language);
 		
-		if (ctype_digit("$number"))
+		if (!$check)
 		{
-			$number = intval($number);
-		}
-		else
-		{
-			$number = floatval($number);
-		}
-		
-		$language = strtolower(trim($language));
-		
-		if (strlen($language) != 2)
-		{
-			error_log(__METHOD__ . ': invalid language parameter.');
-			return 'null';
-		}
-		
-		if (!in_array($language, self::$languages))
-		{
-			error_log(__METHOD__ . ': that language is not yet implemented.');
 			return 'null';
 		}
 		
@@ -387,7 +600,127 @@ class NumberConversion
 		return $text;
 	}
 	
-	private static function parseInt($number, $language, $part)
+	/**
+	 * Convert currency from numeric to linguistic representation.
+	 * 
+	 * @param mixed $amount : the number to convert to the specified language
+	 * @param string $language : a two-letter representation of the language to convert the number to
+	 * @param string $currency : the three-letter currency code (USD, EUR etc.)
+	 * @return string : the currency as written in words in the specified language,
+	 * or "null" if any parameter was invalid
+	 */
+	public static function currencyToWords($amount, $language, $currency)
+	{
+		$check = self::checkParameters($amount, $language);
+		
+		if (!is_string($currency))
+		{
+			error_log('NumberConversion: invalid currency code specified.');
+			$check = false;
+		}
+		
+		$currency = strtoupper(trim($currency));
+		
+		if (!isset(self::$currencies[$currency]))
+		{
+			error_log('NumberConversion: that currency is not yet implemented.');
+			$check = false;
+		}
+		
+		if (!$check)
+		{
+			return 'null';
+		}
+		
+		$amounts = explode('.', "$amount");
+		$wholePart = self::parseInt($amounts[0], $language, 1, $currency);
+		$text = $wholePart . self::getCurrencyName($amounts[0], $currency, $language, '1');
+		
+		if (count($amounts) > 1)
+		{
+			$decimalPart = self::parseInt($amounts[1], $language, 2, $currency);
+			$text .= self::$decimalReplacement[$language]
+				. trim($decimalPart)
+				. self::getCurrencyName($amounts[1], $currency, $language, '2');
+		}
+		
+		return $text;
+	}
+	
+	private static function checkParameters(&$number, &$language)
+	{
+		if (!is_numeric($number))
+		{
+			error_log('NumberConversion: invalid number specified.');
+			return false;
+		}
+		
+		if (ctype_digit("$number"))
+		{
+			$number = intval($number);
+		}
+		else
+		{
+			$number = floatval($number);
+		}
+		
+		$language = strtolower(trim($language));
+		
+		if (strlen($language) != 2)
+		{
+			error_log('NumberConversion: invalid language specified.');
+			return false;
+		}
+		
+		if (!in_array($language, self::$languages))
+		{
+			error_log('NumberConversion: that language is not yet implemented.');
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private static function getCurrencyName($amount, $currency, $language, $part)
+	{
+		$lastDigit = (int)substr("$amount", -1);
+		$lastTwoDigits = (int)substr("$amount", -2);
+		
+		if ($lastDigit === 0)
+		{
+			// 0, 10, 20, 30...
+			return ' ' . self::$currencies[$currency][$language][$part]['3'];
+		}
+		
+		if (($lastTwoDigits > 10)
+			&& ($lastTwoDigits < 20))
+		{
+			// 11-19
+			return ' ' . self::$currencies[$currency][$language][$part]['3'];
+		}
+		
+		if ($lastDigit === 1)
+		{
+			// 1, 21, 31, 41...
+			return ' ' . self::$currencies[$currency][$language][$part]['1'];
+		}
+		
+		if ($language === 'ru')
+		{
+			if ($lastDigit < 5)
+			{
+				// 2, 3, 4, 24, 33... рубля
+				return ' ' . self::$currencies[$currency][$language][$part]['2'];
+			}
+			
+			return ' ' . self::$currencies[$currency][$language][$part]['3'];
+		}
+		
+		// 2-9, 22-29, 33-39...
+		return ' ' . self::$currencies[$currency][$language][$part]['2'];
+	}
+	
+	private static function parseInt($number, $language, $part, $currency = '')
 	{
 		$number = (int)$number;
 		$text = '';
@@ -402,13 +735,13 @@ class NumberConversion
 			&& ($number < 1000000)) // 1000-999 999
 		{
 			$thousands = (int)substr("$number", 0, -3);
-			$text .= self::parseThreeDigitBlock($thousands, $language, $part);
-			$count = (($thousands == 1) ? '1' : '2');
+			$text .= self::parseThreeDigitBlock($thousands, $language, $part, $currency);
+			$count = (($thousands === 1) ? '1' : '2');
 			$text .= ' ' . self::$thousands[$count][$language] . ' ';
 			
 			$number = (int)substr("$number", -3);
 			
-			if ($number == 0)
+			if ($number === 0)
 			{
 				// exact thousands, no hundreds
 				return $text;
@@ -417,13 +750,13 @@ class NumberConversion
 		
 		if ($number < 1000)
 		{
-			$text .= self::parseThreeDigitBlock($number, $language, $part);
+			$text .= self::parseThreeDigitBlock($number, $language, $part, $currency);
 		}
 		
 		return $text;
 	}
 	
-	private static function parseThreeDigitBlock($number, $language, $part)
+	private static function parseThreeDigitBlock($number, $language, $part, $currency = '')
 	{
 		$text = '';
 		
@@ -432,9 +765,14 @@ class NumberConversion
 		{
 			$firstDigit = substr("$number", 0, 1);
 			
-			if ($language == 'en')
+			if ($language === 'en')
 			{
 				$text .= self::$singleDigits[$firstDigit][$language] . self::$en_hundred;
+			}
+			else if (($language === 'es')
+				&& ($number === 100))
+			{
+				$text .= self::$es_hundred;
 			}
 			else
 			{
@@ -443,41 +781,42 @@ class NumberConversion
 			
 			$number = (int)substr("$number", 1);
 			
-			if ($number == 0)
+			if ($number === 0)
 			{
 				// exact hundreds
 				return $text;
 			}
 		}
 		
-		if (($language == 'es') // TODO methinks this doesn't happen... gotta check
-			&& ($number == 100))
+		if ($number < 100)
 		{
-			$text .= ' ' . self::$es_hundred;
-		}
-		else if ($number < 100)
-		{
-			if (($language != 'es')
-				&& ($part == 2))
+			if ($number < 10) // 0-9
 			{
-				$text .= " $number";
-			}
-			else if ($number < 10)
-			{
-				$text .= ' ' . self::$singleDigits[$number][$language]; // 0-9
+				if (($part === 2)
+					&& ($currency === 'RUR')
+					&& (($language === 'lv')
+						|| ($language === 'ru')))
+				{
+					// russian kopeck nouns are feminine gender in certain languages
+					$text .= ' ' . self::$singleDigitsFeminine[$language][$number];
+				}
+				else
+				{
+					$text .= ' ' . self::$singleDigits[$number][$language];
+				}
 			}
 			else if (($number > 10)
 					&& ($number < 20))
 			{
 				$text .= ' ' . self::$teens[$number][$language]; // 11-19
 			}
-			else if ($number % 10 == 0)
+			else if ($number % 10 === 0)
 			{
 				$text .= ' ' . self::$tens[substr($number, 0, 1)][$language];// 10-90
 			}
 			else
 			{
-				if (($language == 'es')
+				if (($language === 'es')
 					&& ($number < 30))
 				{
 					$text .= ' '
@@ -488,8 +827,20 @@ class NumberConversion
 				{
 					$text .= ' '
 						. self::$tens[substr($number, 0, 1)][$language]
-						. self::$spacer[$language]
-						. self::$singleDigits[$number % 10][$language];
+						. self::$spacer[$language];
+					
+					if (($part === 2)
+						&& ($currency === 'RUR')
+						&& (($language === 'lv')
+							|| ($language === 'ru')))
+					{
+						// russian kopeck nouns are feminine gender in certain languages
+						$text .= self::$singleDigitsFeminine[$language][$number % 10];
+					}
+					else
+					{
+						$text .= self::$singleDigits[$number % 10][$language];
+					}
 				}
 			}
 		}
--- test.php
+++ test.php
@@ -1,10 +1,14 @@
 <?php
 require 'NumberConversion.php';
 
-$number = 123456.78;
+echo NumberConversion::currencyToWords(123.45, 'lv', 'EUR'), "\n";
+echo NumberConversion::currencyToWords(234.56, 'lv', 'USD'), "\n";
+echo NumberConversion::currencyToWords(345.67, 'lv', 'LVL'), "\n";
+echo NumberConversion::currencyToWords(456.78, 'lv', 'LTL'), "\n";
+echo NumberConversion::currencyToWords(567.89, 'lv', 'RUR'), "\n";
 
-echo NumberConversion::numberToWords($number, 'lv'), "\n";
-echo NumberConversion::numberToWords($number, 'ru'), "\n";
-echo NumberConversion::numberToWords($number, 'en'), "\n";
-echo NumberConversion::numberToWords($number, 'lt'), "\n";
-echo NumberConversion::numberToWords($number, 'es'), "\n";
+echo NumberConversion::currencyToWords(678.90, 'ru', 'EUR'), "\n";
+echo NumberConversion::currencyToWords(789.01, 'ru', 'USD'), "\n";
+echo NumberConversion::currencyToWords(890.12, 'ru', 'LVL'), "\n";
+echo NumberConversion::currencyToWords(901.23, 'ru', 'LTL'), "\n";
+echo NumberConversion::currencyToWords(12.34, 'ru', 'RUR'), "\n";
