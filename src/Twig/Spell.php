7e0fc6eafd61eb17019513d91d8bc9189293f8d4
mrjanczak <mrjanczak@gmail.com>
2019-09-30 22:14:26 +0200

    spellShort

--- src/Twig/Spell.php
+++ src/Twig/Spell.php
@@ -11,11 +11,11 @@ class Spell extends \Twig\Extension\AbstractExtension
     public function getFilters()
     {
         return [
-            new TwigFilter('spellShort', [$this, 'spellCurrencyAsCode']),
+            new TwigFilter('spellShort', [$this, 'spellShort']),
         ];
     }
 
-    public function spellCurrencyAsCode($amount, $language, $currency)
+    public function spellShort($amount, $language, $currency)
     {
         return Speller::spellCurrencyAsCode($amount, $language, $currency);
     }
