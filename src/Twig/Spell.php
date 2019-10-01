46d508b39ea8b79e7154e55dd5e8dd429c098f75
Juris <jurchiks101@gmail.com>
2019-10-01 21:43:40 +0300

    Adding all the Speller methods as Twig filters.

--- src/Twig/Spell.php
+++ src/Twig/Spell.php
@@ -1,22 +1,33 @@
 <?php
-
 namespace js\tools\numbers2words\Twig;
 
-use Twig\TwigFilter;
 use js\tools\numbers2words\Speller;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 
 class Spell extends AbstractExtension
 {
     public function getFilters()
     {
         return [
+            new TwigFilter('spellNumber', [$this, 'spellNumber']),
             new TwigFilter('spellCurrencyShort', [$this, 'spellCurrencyShort']),
+            new TwigFilter('spellCurrency', [$this, 'spellCurrency']),
         ];
+    }
+
+    public function spellNumber($number, $language)
+    {
+        return Speller::spellNumber($number, $language);
     }
 
     public function spellCurrencyShort($amount, $language, $currency)
     {
         return Speller::spellCurrencyShort($amount, $language, $currency);
+    }
+
+    public function spellCurrency($amount, $language, $currency, $requireDecimal = true, $spellDecimal = false)
+    {
+        return Speller::spellCurrency($amount, $language, $currency, $requireDecimal, $spellDecimal);
     }
 }
