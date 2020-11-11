488f0165bd01b6d8e431bc38b37017e7367b276c
Juris <jurchiks101@gmail.com>
2020-11-12 00:06:29 +0200

    Dropping a redundant check (`$singles` is % 10, so < 10 is guaranteed). Clarifying the else case.

--- src/languages/Lithuanian.php
+++ src/languages/Lithuanian.php
@@ -167,12 +167,12 @@ final class Lithuanian extends Speller
 		{
 			$index = 0;
 		}
-		else if ((($singles > 1) && ($singles < 10)) // 2-9, 22-29, ... 92-99
+		else if (($singles > 1) // 2-9, 22-29, ... 92-99
 			&& (($tens - $singles) !== 10))
 		{
 			$index = 1;
 		}
-		else // 0, 10, 100, 1000
+		else // 0, 10, 11-19, 20, 30, ... 90
 		{
 			$index = 2;
 		}
